b1a6a47512f3ff1a3198cf75326b9c3a57382106
francois Rivolet <francois.rivolet@hotmail.fr>
2017-06-19 15:58:19 +0200

    creation test template

---
+++ src/test/manhattan/Template1Test.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package manhattan;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author fanfan
+ *
+ */
+public class Template1Test {
+
+	@Test
+	public void afficherAscii() {
+		
+	}
+	
+	
+
+}
