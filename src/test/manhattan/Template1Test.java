8878a6fd6fe02f4bc8bdd8d7d42b1ffef7fd8002
francois Rivolet <francois.rivolet@hotmail.fr>
2017-06-19 16:35:41 +0200

    commencement test Template1

--- src/test/manhattan/Template1Test.java
+++ src/test/manhattan/Template1Test.java
@@ -5,6 +5,11 @@ package manhattan;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -12,10 +17,31 @@ import org.junit.Test;
  *
  */
 public class Template1Test {
+	
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
+	@Before
+	public void setUpStreams() {
+	    System.setOut(new PrintStream(outContent));
+	}
+
+	@After
+	public void cleanUpStreams() {
+	    System.setOut(null);
+	}
+	
 	@Test
 	public void afficherAscii() {
 		
+		String mot = "Manhattan";
+		
+		Template1 template = new Template1();
+		
+		
+	    template.afficherAscii(mot);
+
+	    assertEquals("#   # #   # #   # #   # #   #   #     #   #   # #   # ", outContent.toString());
+
 	}
 	
 	
