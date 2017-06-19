ce1f7a2001128caf595fd55917af23063b1277a9
DANIEL-Dereck <daniel.dereck@gmail.com>
2017-06-19 16:39:14 +0200

    correction menu

--- src/core/manhattan/Application.java
+++ src/core/manhattan/Application.java
@@ -8,11 +8,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc1 = new Scanner(System.in);
-		Scanner sc2 = new Scanner(System.in);
-		Scanner sc3 = new Scanner(System.in);
-		Scanner sc4 = new Scanner(System.in);
-
+		Scanner sc = new Scanner(System.in);
 		Faker fake = new Faker(); 
 		int templateSelection = 0;
 		String alphabet;
@@ -24,42 +20,48 @@ public class Application {
 		
 
 		do {
-			System.out.println("Selectionner un template:\n1- Height: 6 Width: 5\n2- Height: 10 Width: 7\n_>");
-			templateSelection = sc1.nextInt();
+			System.out.println("Selectionner un template:\n1- Height: 6 Width: 5\n2- Height: 10 Width: 7");
+			templateSelection = ScannerInt(sc);
 		} while (templateSelection != 1 && templateSelection != 2);
-		System.out.println("Vous avez choisit le template"+templateSelection+"\n");
-		sc1.close();
-
-		System.out.println("Selectionner un alphabet\n_>");
-			alphabet = sc2.nextLine();
+		System.out.println("Vous avez choisit le template: "+templateSelection+"\n");
+		ScannerString(sc);
+		System.out.println("Selectionner un alphabet");
+			alphabet = ScannerString(sc);
 			System.out.println("Vous avez choisit les lettres suivantes: "+alphabet+"\n");
-			sc2.close();
-
+			ScannerString(sc);
 			do {
 				System.out.println("Mode de selection de la chaine a afficher:\n1- Manuel\n2- automatique\n_>");
-				mode = sc3.nextInt();
+				mode = ScannerInt(sc);
 			} while (mode != 1 && mode != 2);
 			System.out.println("Vous avez choisit le mode: "+mode+"\n");
-			sc3.close();
-
+			ScannerString(sc);
 			if (mode == 1) {
-				System.out.println("Saisir une chaine de caracteres a afficher:\n_>");
-				toAff = sc4.nextLine();
+				System.out.println("Saisir une chaine de caracteres a afficher:");
+				toAff = ScannerString(sc);
 				if (templateSelection == 1) {
 					temp1.afficherAscii(toAff);
 				} else {
 					temp2.afficherAscii(toAff);
 				}
-				sc4.close();
 			} else {
-				//here generate string to aff with faker
-				if (templateSelection == 1) {
-					
-					temp1.afficherAscii(fake.lorem().word());//toAff);
+				if (templateSelection == 1) {					
+					temp1.afficherAscii(fake.lorem().word());
 				} else {
-					temp2.afficherAscii(fake.lorem().word());//toAff);
+					temp2.afficherAscii(fake.lorem().word());
 				}
 			}
+			sc.close();
+	}
+	
+	public static String ScannerString(Scanner sc){
+		String value = "";
+		value = sc.nextLine();
+		return value;
 	}
 
+	public static int ScannerInt(Scanner sc){
+		int value = 0;
+		value = sc.nextInt();
+		return value;
+	}
 }
