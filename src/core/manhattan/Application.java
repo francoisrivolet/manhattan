219e38f261ad177cc2bf63ddf8bb0d4fe45b6041
francois Rivolet <francois.rivolet@hotmail.fr>
2017-06-19 14:36:12 +0200

    creation main

---
+++ src/core/manhattan/Application.java
@@ -0,0 +1,10 @@
+package manhattan;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/core/manhattan/Template1.java
@@ -0,0 +1,9 @@
+package manhattan;
+
+import java.util.ArrayList;
+
+public class Template1 {
+	
+	ArrayList<String> a = new ArrayList();
+
+}
